413653b57737c90ffe38168fc304eb495499cd68
ghost122112 <112318377+ghost122112@users.noreply.github.com>
2022-09-06 09:56:30 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+/*
+const message = alert("Hello and Welcome!");
+
+function message2(message2demo) {
+    message2demo = prompt("Type your name", "John");
+    return alert(message2demo);
+}
+alert(message2());
+
+*/
+
+// 5500 port
